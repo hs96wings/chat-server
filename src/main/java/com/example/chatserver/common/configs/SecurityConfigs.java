158f96da6e6b686a4d882ac07aadd0567d459451
hs96wings <hs96wings@gmail.com>
2025-03-10 18:54:49 +0900

    filterchain 설정

---
+++ src/main/java/com/example/chatserver/common/configs/SecurityConfigs.java
@@ -0,0 +1,51 @@
+package com.example.chatserver.common.configs;
+
+import com.example.chatserver.common.auth.JwtAuthFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+public class SecurityConfigs {
+    private final JwtAuthFilter jwtAuthFilter;
+
+    public SecurityConfigs(JwtAuthFilter jwtAuthFilter) {
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain myFilter(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .cors(cors->cors.configurationSource(configurationSource()))
+                .csrf(AbstractHttpConfigurer::disable) // csrf 비활성화
+                .httpBasic(AbstractHttpConfigurer::disable) // HTTP Basic 비활성화
+                // 특정 url 패턴에 대해서는 Authentication 객체 요구하지 않음 (인증처리 제외)
+                .authorizeHttpRequests(a -> a.requestMatchers("/member/create", "/member/doLogin").permitAll().anyRequest().authenticated())
+                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 방식 사용하지 않음
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    CorsConfigurationSource configurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("*")); // 모든 HTTP 메서드 허용
+        configuration.setAllowedHeaders(Arrays.asList("*")); // 모든 Header 값 허용
+        configuration.setAllowCredentials(true); // 자격 증명 허용
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // 모든 url 패턴에 대해 cors 허용 설정
+
+        return source;
+    }
+}
