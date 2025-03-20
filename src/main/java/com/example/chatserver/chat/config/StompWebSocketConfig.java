a1b216cdef5a0a758f33508c1342b0b588af2eef
hs96wings <hs96wings@gmail.com>
2025-03-20 11:21:24 +0900

    stomp config 구성

---
+++ src/main/java/com/example/chatserver/chat/config/StompWebSocketConfig.java
@@ -0,0 +1,29 @@
+package com.example.chatserver.chat.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/connect")
+                .setAllowedOrigins("http://localhost:3000")
+                // ws://가 아닌 http:// 엔드포인트를 사용할 수 있게 해주는
+                // SockJs 라이브러리를 통한 요청을 허용하는 설정
+                .withSockJS();
+    }
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // /publish/1 형태의 메시지 발행해야 함을 설정
+        // /publish로 시작하는 url패턴으로 메시지가 발행되면 @controller 객체의 @MessageMapping 메서드로 라우팅
+        registry.setApplicationDestinationPrefixes("/publish");
+        // /topic/1 형태로 메시지를 수신(subscribe)해야 함을 설정
+        registry.enableSimpleBroker("/topic");
+    }
+
+}
